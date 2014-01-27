bce10743e67e3faedc42c535f7ed55cdc9d89f6d
Adam Stankiewicz <sheerun@sher.pl>
2014-01-27 16:41:16 +0100

    quickix: Do not capture exceptions

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -9,15 +9,6 @@ module API
         error_response(message: e.message, status: 404)
       end
 
-      rescue_from :all do |e|
-        if Rails.env.development?
-          raise e
-        else
-          Raven.capture_exception(e)
-          error_response(message: "Internal server error", status: 500)
-        end
-      end
-
       use API::Logger
     end
   end
